f7a29f6b51eb0767c47ccb83969527cf09010760
gospel328@gmail.com <diddnjs328@naver.com>
2022-07-21 17:58:47 +0900

    2217

--- 2217.py
+++ 2217.py
@@ -2,5 +2,5 @@ words = ["kiwi", "intergral", "leader", "read"]
 res = []
 L = len(words)
 for word in words:
-    res.append(word[-1])
-    if res[]
+    res.append(word[-1]) # res에 word 마지막 글자를 더해준다.
+    if res[] #res의 마지막 글자가 다음 word의 첫글자가 같으면?
